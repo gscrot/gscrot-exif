2b95dd0ab8df4a36fd50b4ba55ae7203aab232a1
redpois0n <red@cmail.nu>
2015-05-15 12:41:08 +0200

    EXIFProcessor

---
+++ src/gscrot/processor/exif/EXIFProcessor.java
@@ -0,0 +1,16 @@
+package gscrot.processor.exif;
+
+import com.redpois0n.gscrot.BinaryImageProcessor;
+
+public class EXIFProcessor extends BinaryImageProcessor {
+
+	public EXIFProcessor() {
+		super("EXIF", null);
+	}
+
+	@Override
+	public byte[] process(byte[] b) {
+		return null;
+	}
+
+}
--- src/gscrot/processor/exif/ExifPlugin.java
+++ src/gscrot/processor/exif/ExifPlugin.java
@@ -1,11 +1,14 @@
 package gscrot.processor.exif;
 
+import com.redpois0n.gscrot.ImageProcessor;
+
 import gscrot.api.Plugin;
 
 public class ExifPlugin extends Plugin {
 
 	public ExifPlugin() {
 		super("EXIF");
+		ImageProcessor.addBinaryProcessor(new EXIFProcessor());
 	}
 
 }
